670b51dd6febbcdaaa19d663ea41a3fd62bfdba3
Gourav Modi <gouravmodi1195@gmail.com>
2024-01-15 23:58:47 +0530

    sec commit

---
+++ todo-app/src/App.js
@@ -0,0 +1,13 @@
+import todoApp from "./components/todoApp";
+// import AI_ML_Enginner from './components/blog2';
+
+function App() {
+  return (
+    <div >
+      {/* <AI_ML_Enginner/> */}
+      <todoApp />
+    </div>
+  );
+}
+
+export default App;
